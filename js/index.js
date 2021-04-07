4bae3c3939d03ea313ba6dcaf164c7c6bc4818c5
Cardozo1w <oscar1w2e@gmail.com>
2021-04-07 17:55:30 -0500

    Avanzando modulo propidad

--- js/index.js
+++ js/index.js
@@ -1,10 +1,17 @@
 const navbar = document.querySelector("#navbar-main");
+const active = document.querySelectorAll('.active');
 
 window.addEventListener("scroll", () => {
   if (window.scrollY > 145) {
     navbar.classList.add("bg-navar-oscuro");
+    navbar.classList.remove('navbar-dark');
+    navbar.classList.add('navbar-light')
+    
   }
   if (window.scrollY < 145) {
     navbar.classList.remove("bg-navar-oscuro");
+    navbar.classList.add('navbar-dark');
+    navbar.classList.remove('navbar-light')
   }
 });
+
